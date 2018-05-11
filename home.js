76e4015cbc1bd6124f62efb8a8113b4a16c2f410
Grizvok <burninglovedesign@gmail.com>
2018-05-10 21:50:51 -0400

    Added async functions to register module

--- home.js
+++ home.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const userRegister = require('./userRegister');
 
 router.get('/', function(req, res) {
 	res.render('home', {
 	});
+});
+
+router.post('/login', function(req, res) {
+	console.log(req.body.email, req.body.password);
+});
+
+router.post('/register', function(req, res) {
+	userRegister.registerUser(req.body.email);
 });
 
 module.exports = router;
--- router.js
+++ router.js
@@ -4,6 +4,7 @@ const router = express.Router();
 
 const home = require('./home');
 
+
 router.use('/', home);
 
 module.exports = router;
