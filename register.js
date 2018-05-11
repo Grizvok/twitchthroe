0185b0f5062f695971ff1375685dbf4ad1c48fd3
Grizvok <burninglovedesign@gmail.com>
2018-05-11 11:33:09 -0400

    Switched out usage of email-validator to the much more full-featured validator

--- register.js
+++ register.js
@@ -4,10 +4,12 @@ const pg = require('pg');
 const scrypt = require('scrypt-for-humans');
 const session = require('express-session');
 const conString = 'postgres://postgres:grizvok5@localhost:5432/scifit';
-const validator = require('email-validator');
+const validator = require('validator');
 
 
 router.post('/', async function(req, res) {
+	//const {login, password, confirmpassword} = req.body;
+	console.log(req.body);
   let emailStatus = await checkUserEmail(req.body.email);
 
   if (emailStatus) {
@@ -20,8 +22,10 @@ router.post('/', async function(req, res) {
 
 function checkUserEmail(email) {
   return new Promise(resolve => {
-    resolve(validator.validate(email));
+    resolve(validator.isEmail(email));
   });
 }
+
+
 
 module.exports = router;
